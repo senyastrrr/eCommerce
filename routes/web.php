f2c11c6d9403ad89480b6b37a3054076b3d67c57
Arseniy <avveeerage@gmail.com>
2024-01-16 14:31:42 +0300

    revert

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
