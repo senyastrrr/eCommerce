20ad2a5dbc10dffbe49575555b3a0bc719128a26
Arseniy <avveeerage@gmail.com>
2024-01-17 18:06:58 +0300

    test: added test for users api

---
+++ tests/Feature/Controllers/API/UserTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Controllers\API;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Database\Factories\RoleFactory;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $role = RoleFactory::new()->create();
+        $this->user = User::factory()->create(['role_id' => $role->id]);
+    }
+
+    public function test_create_user()
+    {
+        $response = $this->postJson('/api/users', [
+            'full_name' => $this->user->full_name,
+            'email' => "example@mail.com",
+            'password' => 'password',
+            'role_id' => $this->user->role_id,
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_get_all_users()
+    {
+        $response = $this->getJson('/api/users');
+        $response->assertStatus(200);
+    }
+
+    public function test_update_user()
+    {
+        $response = $this->putJson("/api/users/{$this->user->id}", [
+            'full_name' => "Grisha",
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', [
+            'id' => $this->user->id,
+            'full_name' => "Grisha",
+        ]);
+    }
+
+
+    public function test_delete_user()
+    {
+        $response = $this->deleteJson("/api/users/{$this->user->id}");
+
+        $response->assertStatus(204);
+        $this->assertDatabaseMissing('users', ['id' => $this->user->id]);
+    }
+}
