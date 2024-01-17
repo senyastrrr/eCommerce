f78597282c09a0fd39adddc530561e8233fea362
Arseniy <avveeerage@gmail.com>
2024-01-17 16:35:09 +0300

    feat: added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,25 @@
 <?php
-
+use App\Http\Controllers\API\UsersController;
+use App\Http\Controllers\API\BillboardsController;
+use App\Http\Controllers\API\CategoriesController;
+use App\Http\Controllers\API\ColorsController;
+use App\Http\Controllers\API\OrderLinesController;
+use App\Http\Controllers\API\OrderStatusesController;
+use App\Http\Controllers\API\OrdersController;
+use App\Http\Controllers\API\ProductAttributesController;
+use App\Http\Controllers\API\ProductConfigurationController;
+use App\Http\Controllers\API\ProductItemsController;
+use App\Http\Controllers\API\ProductItemSizesController;
+use App\Http\Controllers\API\ProductsController;
+use App\Http\Controllers\API\PromotionCategoriesController;
+use App\Http\Controllers\API\PromotionProductsController;
+use App\Http\Controllers\API\PromotionsController;
+use App\Http\Controllers\API\ReviewCommentsController;
+use App\Http\Controllers\API\ReviewsController;
+use App\Http\Controllers\API\RolesController;
+use App\Http\Controllers\API\ShoppingCartItemsController;
+use App\Http\Controllers\API\ShoppingCartsController;
+use App\Http\Controllers\API\SizesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +37,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/user/{id}', [UsersController::class, 'getUserById']);
+Route::apiResource('billboards', BillboardsController::class);
+Route::apiResource('categories', CategoriesController::class);
+Route::apiResource('colors', ColorsController::class);
+Route::apiResource('order-lines', OrderLinesController::class);
+Route::apiResource('order-statuses', OrderStatusesController::class);
+Route::apiResource('orders', OrdersController::class);
+Route::apiResource('product-attributes', ProductAttributesController::class);
+Route::apiResource('product-configuration', ProductConfigurationController::class);
+Route::apiResource('product-items', ProductItemsController::class);
+Route::apiResource('product-item-sizes', ProductItemSizesController::class);
+Route::apiResource('products', ProductsController::class);
+Route::apiResource('promotion-categories', PromotionCategoriesController::class);
+Route::apiResource('promotion-products', PromotionProductsController::class);
+Route::apiResource('promotions', PromotionsController::class);
+Route::apiResource('review-comments', ReviewCommentsController::class);
+Route::apiResource('reviews', ReviewsController::class);
+Route::apiResource('roles', RolesController::class);
+Route::apiResource('shopping-cart-items', ShoppingCartItemsController::class);
+Route::apiResource('shopping-carts', ShoppingCartsController::class);
+Route::apiResource('sizes', SizesController::class);
+
+
+
