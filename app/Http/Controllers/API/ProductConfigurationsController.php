3cdc2b93e996b8214680fa7d63e1eb6096143016
Arseniy <avveeerage@gmail.com>
2024-01-18 18:24:56 +0300

    refactor: changed controller name to plural

---
+++ app/Http/Controllers/API/ProductConfigurationsController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductConfiguration;
+use Illuminate\Http\Request;
+
+class ProductConfigurationsController extends Controller
+{
+    public function index()
+    {
+        return response()->json(ProductConfiguration::all());
+    }
+
+    public function store(Request $request)
+    {
+        $ProductConfiguration = ProductConfiguration::create($request->all());
+        return response()->json($ProductConfiguration, 201);
+    }
+
+    public function show(ProductConfiguration $ProductConfiguration)
+    {
+        return response()->json($ProductConfiguration);
+    }
+
+    public function update(Request $request, ProductConfiguration $ProductConfiguration)
+    {
+        $ProductConfiguration->update($request->all());
+        return response()->json($ProductConfiguration, 200);
+    }
+
+    public function destroy(ProductConfiguration $ProductConfiguration)
+    {
+        $ProductConfiguration->delete();
+        return response()->json(null, 204);
+    }
+}
