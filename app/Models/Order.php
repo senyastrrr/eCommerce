729b0a64f24064c2dcbad69ecf67c10780c44212
Arseniy <avveeerage@gmail.com>
2024-01-18 18:04:12 +0300

    feat: removed timestamps and added cast to created_at

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,8 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    
+    public $timestamps = false;
+
     protected $fillable = [
         'user_id', 'order_status_id', 'created_at', 'shipping_address',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
     ];
 }
