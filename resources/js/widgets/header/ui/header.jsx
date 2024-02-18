cf995a9fb21c5af02bd94eb5ba2537217f6962bd
Arseniy <avveeerage@gmail.com>
2024-02-18 14:58:29 +0300

    feat: finished ui of header component

--- resources/js/widgets/header/ui/header.jsx
+++ resources/js/widgets/header/ui/header.jsx
@@ -1,73 +1,40 @@
 import { MobileMenu } from "@/shared/common/mobile-menu";
-import { Menu } from "@/shared/common/menu";
+import { NavLinks } from "@/shared/common/menu";
 import { ShoppingCart } from "@/entites/cart";
 import React from 'react';
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/shared/ui/dropdown-menu";
-import { Heart } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar";
 import { Link } from '@inertiajs/react';
+import { ProfileButton } from "@/shared/common/profile-button";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import { ActionButton } from "@/shared/common/action-button";
 
-const asset = (path) => `/storage/images/${path}`;
-
-const Header = ({ routes }) => {
+export function Header({ routes }) {
     return (
         <header className="sm:flex sm:justify-between py-2 px-4 sticky top-0 z-30 w-full bg-white">
-            <div className="flex items-center">
-                <MobileMenu routes={routes} />
-                <Link href='/' className="ml-4 lg:ml-0">
-                    <h1 className="text-xl font-bold">STORE NAME</h1>
-                </Link>
-            </div>
-            <Menu routes={routes} />
-            <div className="flex items-center">
-                <ShoppingCart trigger={
-                    <Button
-                        variant="ghost"
-                        size="icon"
-                        className="mr-2 text-gray-700"
-                        aria-label="Custom Shopping Cart Trigger"
-                    >
-                        <ShoppingCartIcon className="h-6 w-6" />
-                    </Button>
-                } />
+            <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full mx-auto w-full max-w-7xl">
+                <div className="flex items-center">
+                    <MobileMenu routes={routes} />
+                    <Link href='/' className="ml-4 lg:ml-0">
+                        <h1 className="text-xl font-bold">STORE NAME</h1>
+                    </Link>
+                </div>
+                <NavLinks routes={routes} />
+                <div className="flex items-center">
+                    <ShoppingCart
+                        trigger={
+                            <ActionButton>
+                                <ShoppingCartOutlinedIcon className="h-6 w-6" />
+                            </ActionButton>
+                        }
+                    />
 
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    aria-label="Toggle Theme"
-                    className="mr-6 text-gray-700"
-                >
-                    <Heart className="w-6 h-6" />
-                </Button>
+                    <ActionButton>
+                        <FavoriteBorderOutlinedIcon className="w-6 h-6" />
+                    </ActionButton>
 
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        <Avatar className="h-8 w-8">
-                            <AvatarImage src={asset("shadcn.jpg")} />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer">Billing</DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer">Subscription</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem className="cursor-pointer">Log Out</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                    <ProfileButton />
+                </div>
             </div>
         </header>
     );
-};
-
-export default Header;
+}
