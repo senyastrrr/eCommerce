6529721c0ea60e95ddb9fcaeef399960be1dc2bc
Arseniy <avveeerage@gmail.com>
2024-02-18 13:25:57 +0300

    feat: added mobile menu component

---
+++ resources/js/shared/common/mobile-menu/ui/mobile-menu.jsx
@@ -0,0 +1,21 @@
+import { Link } from "@inertiajs/react";
+import { Sheet, SheetTrigger, SheetContent } from "../../shared/ui/sheet";
+
+export default function MobileMenu({ routes }) {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Menu className="h-6 md:hidden w-6" />
+            </SheetTrigger>
+            <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+                <nav className="flex flex-col gap-4">
+                    {routes.map((route, i) => (
+                        <Link key={i} href={route.href} className="block px-2 py-1 text-lg">
+                            {route.label}
+                        </Link>
+                    ))}
+                </nav>
+            </SheetContent>
+        </Sheet>
+    );
+};
