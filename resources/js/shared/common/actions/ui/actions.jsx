b6ee4b6ee81357f553b61ab61e744bac9c7ef341
Arseniy <avveeerage@gmail.com>
2024-02-18 21:34:41 +0300

    chore: changed actions export

--- resources/js/shared/common/actions/ui/actions.jsx
+++ resources/js/shared/common/actions/ui/actions.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Actions = ({ children }) => {
+export function Actions({ children }) {
     return (
         <div className="absolute top-4 right-4 flex space-x-2">
             <React.Fragment>
@@ -8,6 +8,4 @@ const Actions = ({ children }) => {
             </React.Fragment>
         </div>
     );
-};
-
-export default Actions;
+}
