2a0adac66db46c12711e9561159417f68af9d57d
Arseniy <avveeerage@gmail.com>
2024-02-18 13:43:35 +0300

    feat: added menu component

---
+++ resources/js/shared/common/menu/ui/menu.jsx
@@ -0,0 +1,19 @@
+import { Link } from '@inertiajs/react';
+import { Button } from "@/shared/ui/button";
+
+export function Menu({ routes }) {
+    return (
+        <nav className="mx-auto flex items-center space-x-4 lg:space-x-4 hidden md:block">
+            {routes.map((route, i) => (
+                <Button asChild variant="ghost" key={i}>
+                    <Link
+                        href={route.href}
+                        className="text-sm font-medium transition-colors"
+                    >
+                        {route.label}
+                    </Link>
+                </Button>
+            ))}
+        </nav>
+    );
+};
