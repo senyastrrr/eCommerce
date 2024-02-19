d85e7a961785bef6f74f2263f4f56a674d87a6f1
Arseniy <avveeerage@gmail.com>
2024-02-19 11:27:32 +0300

    chore moved products-grid to widgets

---
+++ resources/js/widgets/products-grid/ui/products-grid.jsx
@@ -0,0 +1,14 @@
+import { ProductCard } from "@/entites/product/product-card";
+
+export function ProductsGrid({ title, products }) {
+  return (
+    <div className='mt-8 rounded-xl bg-gray-100 pb-12'>
+      <h1 className="text-2xl font-bold mb-8 pt-8 pl-8">{title}</h1>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 px-4">
+        {products.map((product, index) => (
+          <ProductCard product={product} discount={index}/>
+        ))}
+      </div>
+    </div>
+  );
+}
