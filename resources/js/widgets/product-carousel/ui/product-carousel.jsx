dd23dbe4c987980ee1703738467df61aef3204d4
Arseniy <avveeerage@gmail.com>
2024-02-21 12:45:58 +0300

    feat: added product-carousel component

---
+++ resources/js/widgets/product-carousel/ui/product-carousel.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { CustomCarousel } from '@/shared/common/carousel';
+import { CardContent } from '@/shared/ui/card';
+import { CarouselItem } from '@/shared/ui/carousel';
+import { Card } from '@material-tailwind/react';
+
+const asset = (path) => `/storage/images/${path}`;
+
+export function ProductCarousel() {
+    return (
+        <CustomCarousel
+        className="flex w-1/3"
+        isButtonsVisible={true}
+        isLooped={true}
+      >
+        {Array.from({ length: 4 }).map((_, index) => (
+          <CarouselItem key={index} className="p-0">
+            <Card className="border-0 p-0">
+              <CardContent className="p-0">
+                <img src={asset(`product-0${index + 1}.jpg`)} alt="" />
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CustomCarousel>
+    );
+}
