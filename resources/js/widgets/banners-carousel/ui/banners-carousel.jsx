c12c5efeb90185c09831c74983548dbd5396c58c
Arseniy <avveeerage@gmail.com>
2024-02-26 18:37:04 +0300

    feat: added data fetch to banners-carousel

--- resources/js/widgets/banners-carousel/ui/banners-carousel.jsx
+++ resources/js/widgets/banners-carousel/ui/banners-carousel.jsx
@@ -1,27 +1,45 @@
+import { useBillboards } from '@/entites/banner/api/queries';
 import { CustomCarousel, AutoplayPlugin } from '@/shared/common/carousel';
 import { Card, CardContent } from '@/shared/ui/card';
 import { CarouselItem } from '@/shared/ui/carousel';
+import Autoplay from "embla-carousel-autoplay"
 import React from 'react';
 
 const asset = (path) => `/storage/images/${path}`;
 
 export function BannersCarousel() {
-    return (
-        <CustomCarousel
-            className="flex w-screen"
-            isButtonsVisible={false}
-            isLooped={true}
-            plugin={AutoplayPlugin(5000, false)}
-        >
-            {Array.from({ length: 4 }).map((_, index) => (
-                <CarouselItem key={index} className="p-0">
-                    <Card className="border-0 p-0">
-                        <CardContent className="p-0">
-                            <img src={asset(`slide-0${index + 1}.jpg`)} alt="" />
-                        </CardContent>
-                    </Card>
-                </CarouselItem>
-            ))}
-        </CustomCarousel>
-    );
+
+    const bannersQueries = useBillboards();
+    const plugin = React.useRef(Autoplay({ delay: 5000, stopOnInteraction: true }));
+    
+    if (bannersQueries.isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (bannersQueries.isError) {
+        return <div>Error...</div>;
+    }
+
+    if (bannersQueries.isSuccess) {
+        return (
+            <CustomCarousel
+                className="flex w-screen"
+                isButtonsVisible={false}
+                isLooped={true}
+                plugin={plugin}
+            >
+                {bannersQueries.data.map((banner) => (
+                    banner.isActual ? (
+                        <CarouselItem key={banner?.id} className="p-0">
+                            <Card className="border-0 p-0">
+                                <CardContent className="p-0">
+                                    <img src={asset(banner.image)} alt={banner.image} />
+                                </CardContent>
+                            </Card>
+                        </CarouselItem>
+                    ) : null
+                ))}
+
+            </CustomCarousel>
+        );
+    }
 }
