249e7bf32fb9fa14fcfdc584999fa5699f63a128
Arseniy <avveeerage@gmail.com>
2024-03-06 14:44:48 +0300

    feat: added is employee middleware

---
+++ app/Http/Middleware/isEmployee.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Role;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class isEmployee
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()) {
+            $role = Role::find(Auth::user()->role_id);
+            if ($role && $role == 'Employee')
+                return $next($request);
+        } else {
+            abort(403, 'Unauthorized action.');
+        }
+    }
+}
