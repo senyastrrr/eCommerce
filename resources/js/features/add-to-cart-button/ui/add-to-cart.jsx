701522bb8c8bf4e80484a22b9ee2e40e5cf4ef60
Arseniy <avveeerage@gmail.com>
2024-03-19 14:20:47 +0300

    feat: implemented add-to-cart feature

--- resources/js/features/add-to-cart-button/ui/add-to-cart.jsx
+++ resources/js/features/add-to-cart-button/ui/add-to-cart.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import { Button } from '@/shared/ui/button';
+import { useCreateShoppingCartItem } from '@/entites/shopping-cart-item';
+import { useProductItemSizeByProductAndSize } from '@/entites/product-item-size';
+import { useShoppingCartByUserId } from '@/entites/shopping-cart';
+import { usePage } from '@inertiajs/react';
 
-export function AddToCartButton() {
+export function AddToCartButton({ formData }) {
+
+    const { user } = usePage().props.auth;
+    const createShoppingCartItem = useCreateShoppingCartItem();
+    const product = useProductItemSizeByProductAndSize(formData.product_id, formData.size_id);
+    const shoppingCart = useShoppingCartByUserId(user?.id);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            if (product.isSuccess && shoppingCart.isSuccess)
+                createShoppingCartItem.mutate({ product_id: product.data.id, cart_id: shoppingCart.data.id, qty: 1 })
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <Button>Add to cart</Button>
+        <Button onClick={handleSubmit}>Добавить в корзину</Button>
     );
 }
