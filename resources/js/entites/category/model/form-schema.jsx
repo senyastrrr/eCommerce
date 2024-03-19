18c9094bf478c761b8eeafd5725934039dc150a2
Arseniy <avveeerage@gmail.com>
2024-03-19 14:12:54 +0300

    fix: fixed id types

--- resources/js/entites/category/model/form-schema.jsx
+++ resources/js/entites/category/model/form-schema.jsx
@@ -2,7 +2,7 @@ import * as z from "zod"
 
 export const formSchema = z.object({
     id: z.number().optional().nullable(),
-    parent_id: z.nullable(z.string()),
+    parent_id: z.coerce.number(),
     name: z.string().min(1),
     description: z.string().min(1),
     discount: z.coerce.number().optional().nullable(),
--- resources/js/entites/product-item/model/form-schema.jsx
+++ resources/js/entites/product-item/model/form-schema.jsx
@@ -2,8 +2,8 @@ import * as z from "zod"
 
 export const formSchema = z.object({
     id: z.number().optional().nullable(),
-    color_id: z.nullable(z.string()),
-    product_id: z.nullable(z.string()),
+    color_id: z.coerce.number(),
+    product_id: z.coerce.number(),
     SKU: z.string(),
     image: z.string().min(1),
 });
