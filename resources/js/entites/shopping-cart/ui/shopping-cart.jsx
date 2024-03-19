76c7a5b1e38a280d83f07bd3593d5a9293bab6d9
Arseniy <avveeerage@gmail.com>
2024-03-19 14:19:56 +0300

    feat: added data fetch

--- resources/js/entites/shopping-cart/ui/shopping-cart.jsx
+++ resources/js/entites/shopping-cart/ui/shopping-cart.jsx
@@ -1,41 +1,36 @@
-import { Input } from "@/shared/ui/input";
-import { Label } from "@/shared/ui/label";
 import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription, SheetFooter, SheetClose } from "@/shared/ui/sheet";
 import { Button } from "@/shared/ui/button";
+import { ShoppingCartItem, useShoppingCartItemsByUserId } from "@/entites/shopping-cart-item";
+import { usePage } from "@inertiajs/react";
 
 export function ShoppingCart({ trigger }) {
-    return (
-        <Sheet>
-            <SheetTrigger asChild>
-                { trigger }
-            </SheetTrigger>
-            <SheetContent side="right">
-                <SheetHeader>
-                    <SheetTitle>Edit profile</SheetTitle>
-                    <SheetDescription>
-                        Make changes to your profile here. Click save when you're done.
-                    </SheetDescription>
-                </SheetHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Name
-                        </Label>
-                        <Input id="name" className="col-span-3" />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
-                        </Label>
-                        <Input id="username" className="col-span-3" />
-                    </div>
-                </div>
-                <SheetFooter>
-                    <SheetClose asChild>
-                        <Button type="submit">Save changes</Button>
-                    </SheetClose>
-                </SheetFooter>
-            </SheetContent>
-        </Sheet>
-    );
+
+    const { user } = usePage().props.auth;
+    const items = useShoppingCartItemsByUserId(user?.id)
+
+    if (items.isSuccess)
+        return (
+            <Sheet>
+                <SheetTrigger asChild>
+                    {trigger}
+                </SheetTrigger>
+                <SheetContent side="right" className="overflow-y-auto">
+                    <SheetHeader>
+                        <SheetTitle>Корзина</SheetTitle>
+                        <SheetDescription>
+                            Здесь отображаются ваши товары, добавленные в корзину.
+                        </SheetDescription>
+                    </SheetHeader>
+                    {items.data.map((item) => (
+                        <ShoppingCartItem key={item.product_id} product_id={item.product_id} item={item.id}/>
+                    ))}
+                    
+                    <SheetFooter className="mt-6">
+                        <SheetClose asChild>
+                            <Button type="submit">Перейти к оформлению</Button>
+                        </SheetClose>
+                    </SheetFooter>
+                </SheetContent>
+            </Sheet>
+        );
 }
