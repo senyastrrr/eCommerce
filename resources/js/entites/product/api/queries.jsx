909c0ae2af365bb48bb1ab70e820c80ad866b63b
Arseniy <avveeerage@gmail.com>
2024-03-19 14:19:24 +0300

    feat: added useFeaturedProducts

--- resources/js/entites/product/api/queries.jsx
+++ resources/js/entites/product/api/queries.jsx
@@ -17,6 +17,21 @@ export function useDiscountedProducts(){
     });
 }
 
+export function useFeaturedProducts(){
+    return useQuery({
+        queryKey: [`featured-${_queryKey}`],
+        queryFn: () => get(`featured-products`),
+        refetchOnWindowFocus: false,
+    });
+}
+
+export function useProductColors(id) {
+    return useQuery({
+        queryKey: [`${_queryKey}-colors` , id],
+        queryFn: () => get(`${_root}/get-product-colors/${id}`)
+    })
+}
+
 export function useProducts() {
     return useQuery({
         queryKey: [_queryKey],
