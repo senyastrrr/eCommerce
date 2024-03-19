ef3723051cb820f878436ab3e3367a27f8baa1dd
Arseniy <avveeerage@gmail.com>
2024-03-19 14:15:49 +0300

    feat: added data fetch

--- resources/js/widgets/product-images/ui/product-images.jsx
+++ resources/js/widgets/product-images/ui/product-images.jsx
@@ -1,13 +1,12 @@
+import { asset } from '@/shared/lib/storage-path';
 import React from 'react';
 
-const asset = (path) => `/storage/images/${path}`;
-
-export function ProductImages() {
+export function ProductImages({ products }) {
     return (
-        <div className="flex flex-col flex-wrap gap-12 hidden md:flex">
-            {Array.from({ length: 4 }).map((_, index) => (
-                <div key={index} style={{ width: 100, height: 100 }}>
-                    <img src={asset(`product-0${index + 1}.jpg`)} alt={`image-${index}`} />
+        <div className="flex flex-col flex-wrap gap-20 hidden md:flex">
+            {products.map((product, index) => (
+                <div key={index} style={{ width: 150, height: 150 }}>
+                    <img src={asset(product.image)} alt={product.SKU} />
                 </div>
             ))}
         </div>
