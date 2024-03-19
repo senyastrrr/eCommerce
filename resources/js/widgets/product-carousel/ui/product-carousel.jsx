c274ec131b969eba9e4f328065a92cee07b1e0dd
Arseniy <avveeerage@gmail.com>
2024-03-19 14:16:05 +0300

    chore: moved asset to shared

--- resources/js/widgets/product-carousel/ui/product-carousel.jsx
+++ resources/js/widgets/product-carousel/ui/product-carousel.jsx
@@ -3,25 +3,24 @@ import { CustomCarousel } from '@/shared/common/carousel';
 import { CardContent } from '@/shared/ui/card';
 import { CarouselItem } from '@/shared/ui/carousel';
 import { Card } from '@material-tailwind/react';
+import { asset } from '@/shared/lib/storage-path';
 
-const asset = (path) => `/storage/images/${path}`;
-
-export function ProductCarousel() {
-    return (
-        <CustomCarousel
-        className="flex w-1/3 min-w-80"
-        isButtonsVisible={true}
-        isLooped={true}
-      >
-        {Array.from({ length: 4 }).map((_, index) => (
-          <CarouselItem key={index} className="p-0">
-            <Card className="border-0 p-0">
-              <CardContent className="p-0">
-                <img src={asset(`product-0${index + 1}.jpg`)} alt="" className='rounded-lg'/>
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))}
-      </CustomCarousel>
-    );
+export function ProductCarousel({ products }) {
+  return (
+    <CustomCarousel
+      className="flex w-1/3 min-w-80"
+      isButtonsVisible={true}
+      isLooped={true}
+    >
+      {products.map((product, index) => (
+        <CarouselItem key={index} className="p-0">
+          <Card className="border-0 p-0">
+            <CardContent className="p-0">
+              <img src={asset(product.image)} alt={product.SKU} className='rounded-lg' />
+            </CardContent>
+          </Card>
+        </CarouselItem>
+      ))}
+    </CustomCarousel>
+  );
 }
