681cd5baef5025931d98609fbf2b5f3a341403e3
Arseniy <avveeerage@gmail.com>
2024-03-19 14:09:45 +0300

    feat: added getByProductItemAndSize

--- app/Http/Controllers/API/ProductItemSizesController.php
+++ app/Http/Controllers/API/ProductItemSizesController.php
@@ -49,4 +49,43 @@ class ProductItemSizesController extends Controller
 
         return response()->json($sizes, 200);
     }
+
+    public function getByProductItemAndSize($id, $size)
+    {
+        $productItemSize =  ProductItemSize::where('product_id', $id)->where('size_id', $size)->where('qty', '>', '0')->first();
+        return response()->json($productItemSize, 200);
+    }
+
+    public function getProductItemSizeDetails($id)
+    {
+        $productItemSize = ProductItemSize::findOrFail($id);
+        $productItem = $productItemSize->productItem;
+
+        // Проверяем, существует ли связанный продукт
+        if (!$productItem) {
+            return response()->json(['error' => 'Product not found'], 404);
+        }
+        $product = $productItem->product;
+
+        // Проверяем, существует ли связанный продукт для product item
+        if (!$product) {
+            return response()->json(['error' => 'Product not found'], 404);
+        }
+
+        $productName = $product->name;
+        $productImage = $productItem->image;
+        $productColor = $productItem->color->name;
+        $productSize = $productItemSize->size()->pluck('name');
+        $productQuantity = $productItemSize->qty;
+
+        $productDetails = [
+            'name' => $productName,
+            'image' => $productImage,
+            'color' => $productColor,
+            'size' => $productSize,
+            'quantity' => $productQuantity,
+        ];
+
+        return response()->json($productDetails, 200);
+    }
 }
