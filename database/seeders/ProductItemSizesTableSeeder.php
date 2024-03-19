b3188ea79f52aad6c5a968e91172db1bfea4ce6b
Arseniy <avveeerage@gmail.com>
2024-03-19 14:11:47 +0300

    fix: fixed repetitive sizes

--- database/seeders/ProductItemSizesTableSeeder.php
+++ database/seeders/ProductItemSizesTableSeeder.php
@@ -19,20 +19,30 @@ class ProductItemSizesTableSeeder extends Seeder
     {
         $faker = Faker::create();
         $products = ProductItem::all();
-        $sizes = Size::all();
+        $sizes = Size::all()->shuffle(); // Shuffle the sizes collection
 
         foreach ($products as $product) {
             $productSizeQuantities = [];
 
-            for ($i = 0; $i < $faker->numberBetween(2, 6); $i++) {
-                $size = $sizes->random();
+            // Iterate through a shuffled collection of sizes
+            foreach ($sizes as $size) {
                 $quantity = $faker->numberBetween(1, 5);
 
-                $productSizeQuantities[] = [
-                    'product_id' => $product->id,
-                    'size_id' => $size->id,
-                    'qty' => $quantity,
-                ];
+                $sizeId = $size->id;
+
+                // Check if the size has already been added to the array
+                if (!isset($productSizeQuantities[$sizeId])) {
+                    $productSizeQuantities[$sizeId] = [
+                        'product_id' => $product->id,
+                        'size_id' => $sizeId,
+                        'qty' => $quantity,
+                    ];
+
+                    // Ensure each product gets a unique size
+                    if (count($productSizeQuantities) >= $faker->numberBetween(2, 6)) {
+                        break;
+                    }
+                }
             }
 
             DB::table('product_item_sizes')->insert($productSizeQuantities);
