50a5562df3eb6ec15a13b01dd954da30d75b9958
Arseniy <avveeerage@gmail.com>
2024-03-19 14:20:58 +0300

    feat: added header

--- resources/js/Pages/ProductDetails.jsx
+++ resources/js/Pages/ProductDetails.jsx
@@ -1,4 +1,6 @@
 import { useProductItemsByProductId } from '@/entites/product-item/api/queries';
+import { home } from '@/shared/routes/home-routes';
+import { Header } from '@/widgets/header';
 import { ProductCarousel } from '@/widgets/product-carousel';
 import { ProductImages } from '@/widgets/product-images';
 import { ProductInfo } from '@/widgets/product-info';
@@ -8,15 +10,19 @@ import React from 'react';
 function ProductDetails() {
 
   const id = usePage().props.id;
+  const discount = usePage().props.discount;
   const productItems = useProductItemsByProductId(id);
 
   if (productItems.isSuccess)
     return (
-      <div className='flex mx-10 mt-8 md:flex-row gap-12 justify-center flex-col'>
-        <ProductImages />
-        <ProductCarousel />
-        <ProductInfo productId={id} />
-      </div>
+      <>
+        <Header routes={home} />
+        <div className='flex mx-10 mt-8 md:flex-row gap-12 justify-center flex-col'>
+          <ProductImages products={productItems.data} />
+          <ProductCarousel products={productItems.data} />
+          <ProductInfo productId={id} discount={discount} />
+        </div>
+      </>
     );
 }
 export default ProductDetails;
